a4e3c60fcdeab15a8dbb8bb1cd7536ff13bc6388
Carlos Gizbert <carlos.gizbert@zapsign.com.br>
2024-08-12 21:29:47 -0300

    feat: pass styles

--- src/pages/authenticated/home/modal/register/schema.ts
+++ src/pages/authenticated/home/modal/register/schema.ts
@@ -1,13 +1,22 @@
-import { z } from 'zod';
+import { z } from "zod";
 
-export const registerFormSchema = z.object({
-  username: z.string().min(1, 'Preencha o nome completo'),
-  email: z.string().email('Digite um email válido'),
-  password: z.string().min(6, 'Digite pelo menos 6 caracteres'),
-  confirmPassword: z.string().min(6, 'Digite pelo menos 6 caracteres'),
-}).refine((data) => data.password === data.confirmPassword, {
-  message: "As senhas não coincidem",
-  path: ["confirmPassword"],  
-});
+export const registerFormSchema = z
+  .object({
+    username: z
+      .string()
+      .min(3, "Digite um nome maior")
+      .max(30, "Digite um nome menor")
+      .regex(/^[A-Za-z\s]+$/, "O nome deve conter apenas letras")
+      .refine((value) => value.trim().split(" ").length >= 2, {
+        message: "Digite pelo menos um nome e um sobrenome",
+      }),
+    email: z.string().email("Digite um email válido"),
+    password: z.string().min(6, "Digite pelo menos 6 caracteres"),
+    confirmPassword: z.string().min(6, "Digite pelo menos 6 caracteres"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "As senhas não coincidem",
+    path: ["confirmPassword"],
+  });
 
 export type RegisterFormType = z.infer<typeof registerFormSchema>;
